7deeee4ba0c99fae51e0b82b37a07e68ec552815
Basit-Technolligence <pakkobra007@gmail.com>
2020-04-12 16:23:51 +0500

    latest

--- app/src/main/java/com/example/videoaudioplayer/VideoFolderActivity.java
+++ app/src/main/java/com/example/videoaudioplayer/VideoFolderActivity.java
@@ -1,11 +1,15 @@
 package com.example.videoaudioplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 
@@ -21,7 +25,13 @@ public class VideoFolderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_video_folder);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                recreate();
+                return;
+            }
+        }
         recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         fetchVideoFolders();
--- app/src/main/java/com/example/videoaudioplayer/ViewVideo.java
+++ app/src/main/java/com/example/videoaudioplayer/ViewVideo.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -14,11 +16,14 @@ import java.util.ArrayList;
 public class ViewVideo extends AppCompatActivity {
     VideoView videoView;
     private View decorView;
-    ImageView backArrow;
+    ImageView backArrow,screenLock,previous,next,play,pause,fullScreen,smallScreen,cropScreen,rotateScreen;
     TextView videoTitleText;
     ArrayList<String> videoUri;
     ArrayList<String> videoTitle;
     int position;
+    RelativeLayout mainLayout;
+    FrameLayout topFrameLayout,bottomFrameLayout;
+    boolean framesVisibility=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +32,52 @@ public class ViewVideo extends AppCompatActivity {
         videoTitleText=(TextView) findViewById(R.id.videoTitle);
         backArrow=(ImageView) findViewById(R.id.backArrow);
         videoView=(VideoView) findViewById(R.id.videoView);
+        screenLock=(ImageView) findViewById(R.id.screenLock);
+        previous=(ImageView) findViewById(R.id.previous);
+        next=(ImageView) findViewById(R.id.next);
+        play=(ImageView)findViewById(R.id.play);
+        pause=(ImageView)findViewById(R.id.pause);
+        fullScreen=(ImageView) findViewById(R.id.fullScreen);
+        smallScreen=(ImageView) findViewById(R.id.smallScreen);
+        cropScreen=(ImageView) findViewById(R.id.cropScreen);
+        rotateScreen=(ImageView) findViewById(R.id.rotateScreen);
+        topFrameLayout =(FrameLayout) findViewById(R.id.topFrameLayout);
+        bottomFrameLayout=(FrameLayout) findViewById(R.id.bottomFrameLayout);
+        mainLayout=(RelativeLayout) findViewById(R.id.mainLayout);
         videoUri=getIntent().getStringArrayListExtra("VideoUri");
         videoTitle=getIntent().getStringArrayListExtra("VideoTitle");
         position=getIntent().getIntExtra("VideoPosition",0);
         videoTitleText.setText(videoTitle.get(position));
         videoView.setVideoURI(Uri.parse(videoUri.get(position)));
         videoView.start();
+        topFrameLayout.setVisibility(View.GONE);
+        bottomFrameLayout.setVisibility(View.GONE);
+        play.setVisibility(View.GONE);
+        cropScreen.setVisibility(View.GONE);
+        smallScreen.setVisibility(View.GONE);
+        rotateScreen.setVisibility(View.GONE);
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(framesVisibility){
+                    topFrameLayout.setVisibility(View.GONE);
+                    bottomFrameLayout.setVisibility(View.GONE);
+                    rotateScreen.setVisibility(View.GONE);
+                    framesVisibility=false;
+                }else{
+                    topFrameLayout.setVisibility(View.VISIBLE);
+                    bottomFrameLayout.setVisibility(View.VISIBLE);
+                    rotateScreen.setVisibility(View.VISIBLE);
+                    framesVisibility=true;
+                }
+            }
+        });
+        //videoView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

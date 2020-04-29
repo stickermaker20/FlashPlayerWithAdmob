7e4b54aadd7c18270e25fab0395780386ebf63a1
Basit-Technolligence <pakkobra007@gmail.com>
2020-04-29 23:03:37 +0500

    ads added

--- app/src/main/java/com/example/videoaudioplayer/VideoList.java
+++ app/src/main/java/com/example/videoaudioplayer/VideoList.java
@@ -3,6 +3,7 @@ package com.example.videoaudioplayer;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import wseemann.media.FFmpegMediaMetadataRetriever;
 
 import android.content.Intent;
 import android.database.Cursor;
@@ -28,6 +29,8 @@ public class VideoList extends AppCompatActivity {
     ArrayList<String> videosTitle= new ArrayList<>();
     ArrayList<String> videosThumb= new ArrayList<>();
     ArrayList<String> videosDuration= new ArrayList<>();
+    //new lib for duration
+    FFmpegMediaMetadataRetriever mFFmpegMediaMetadataRetriever = new FFmpegMediaMetadataRetriever();
 
 
     @Override
@@ -47,7 +50,8 @@ public class VideoList extends AppCompatActivity {
     }
     private void fetchVideos(String folderName) {
         Uri uri;
-        int column_index_data,thum,title,duration;
+        int column_index_data,thum,title;//duration;
+        String mVideoDuration;
         Cursor cursor;
         uri=MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
         String[] projection={MediaStore.MediaColumns.DATA, MediaStore.Video.VideoColumns.DURATION, MediaStore.Video.Media.DISPLAY_NAME, MediaStore.Video.Media._ID, MediaStore.Video.Thumbnails.DATA};
@@ -61,13 +65,16 @@ public class VideoList extends AppCompatActivity {
 
         column_index_data=cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
         title=cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME);
-        duration=cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
+        //duration=cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
         thum=cursor.getColumnIndexOrThrow(MediaStore.Video.Thumbnails.DATA);
         while (cursor.moveToNext()){
             videosUri.add(cursor.getString(column_index_data));
             videosThumb.add(cursor.getString(thum));
             videosTitle.add(cursor.getString(title));
-            videosDuration.add(cursor.getString(duration));
+            //new code for duration
+            mFFmpegMediaMetadataRetriever.setDataSource(cursor.getString(column_index_data));
+            mVideoDuration = mFFmpegMediaMetadataRetriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_DURATION);
+            videosDuration.add(mVideoDuration);
 
         }
         recyclerView.setAdapter(new VideoListAdapter(videosUri,videosThumb,videosTitle,videosDuration,VideoList.this));
